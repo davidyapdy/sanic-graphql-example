e7457dd11eb1a47b918794dcfb5854f5d2c4003e
David <davidyap93@gmail.com>
2017-03-23 15:29:11 +0800

    Refactor

--- app.py
+++ app.py
@@ -28,8 +28,10 @@ default_query = """
 }
 """.strip()
 
-# It's like HTTP routes but instead, it directs you to /graphql (Schema). graphiq = True will initiate the GUI
-app.add_route(GraphQLView.as_view(schema=schema, graphql=True, graphiq=True), '/graphql')
+# It's like HTTP routes but instead, it directs you to /graphql (Schema).
+# graphiq = True will initiate the GUI
+app.add_route(GraphQLView.as_view(
+    schema=schema, graphql=True, graphiq=True), '/graphql')
 
 # runs the app and database. init_db contains seed data.
 if __name__ == '__main__':
