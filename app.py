0a47b9edf46a8412f6992e164e6acc5ed9a453cf
David Yap <davidyap93@gmail.com>
2017-03-23 15:12:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from practice_files.graphane_practice.app.schema import schema
 app = Sanic(__name__)
 app.debug = True
 
+# This will be the query
 default_query = """
 {
     allEmployee {
@@ -27,8 +28,10 @@ default_query = """
 }
 """.strip()
 
+# It's like HTTP routes but instead, it directs you to /graphql (Schema)
 app.add_route(GraphQLView.as_view(schema=schema, graphql=True, graphiq=True), '/graphql')
 
+# runs the app and database. init_db contains seed data.
 if __name__ == '__main__':
     init_db()
     app.run()
